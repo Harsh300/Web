c22efe3a5b539c6eb920bec4b3f108f47ac62a5b
Rohil <rohil.arya@uoit.net>
2018-04-08 17:17:12 -0400

    Twitter live feed working

--- MusicPlaya.js
+++ MusicPlaya.js
@@ -41,6 +41,26 @@ $(document).ready(function() {
 		fetch(spotifySearchURL,{
 			headers:{'Authorization': 'Bearer'+ accessToken}
 		}).then((response)=>response.json()).then(data=>console.log(data));
+		
+		//Twitter
+		
+	window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}
+(document, "script", "twitter-wjs"));
 
 	});
 
